4e093f1449fc6f631551a84cf2efa91105a8de10
Kai Hendry <hendry@goserverless.sg>
2018-02-21 12:57:34 +0800

    Do not index beta.goserverless.sg

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"html/template"
 
@@ -29,18 +30,29 @@ func main() {
 	}
 }
 
+func countryFlag(x string) string {
+	if len(x) != 2 {
+		return ""
+	}
+	if x[0] < 'A' || x[0] > 'Z' || x[1] < 'A' || x[1] > 'Z' {
+		return ""
+	}
+	return string(0x1F1E6+rune(x[0])-'A') + string(0x1F1E6+rune(x[1])-'A')
+}
+
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	// signup_form.tmpl just needs a {{ .csrfField }} template tag for
-	// csrf.TemplateField to inject the CSRF token into. Easy!
+
+	// Don't index beta.goserverless.sg
+	if os.Getenv("UP_STAGE") != "production" {
+		w.Header().Set("X-Robots-Tag", "none")
+	}
+
 	t := template.Must(template.New("").ParseGlob("templates/*.html"))
 	t.ExecuteTemplate(w, "index.html", map[string]interface{}{
 		csrf.TemplateTag: csrf.TemplateField(r),
-		"Header":         r.Header,
+		"Stage":          os.Getenv("UP_STAGE"),
+		"EmojiCountry":   countryFlag(strings.Trim(r.Header.Get("Cloudfront-Viewer-Country"), "[]")),
 	})
-	// We could also retrieve the token directly from csrf.Token(r) and
-	// set it in the request header - w.Header.Set("X-CSRF-Token", token)
-	// This is useful if you're sending JSON to clients or a front-end JavaScript
-	// framework.
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
